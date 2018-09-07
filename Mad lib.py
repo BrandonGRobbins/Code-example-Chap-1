942e93a06675e50e9f773dfdaa6db2d34dd0e771
Brandon Robbins <brandon.robbins@tooeleschools.org>
2018-09-07 13:25:48 -0600

    4/5th code example chap 1

---
+++ Mad lib.py
@@ -0,0 +1,43 @@
+#print("hi class")
+
+#first_name="Brandon"
+
+
+#fact=" i like videogames"
+#print("my name is",first_name,"and",fact)
+#message= first_name+fact
+#print(message)
+
+#num1=1
+#num2=2
+#answer=num1 +num2 +num1*num1
+#print(answer)
+
+word1=input("enter name")
+word2="quickly"
+word3="run"
+word4="pool"
+word5="takes"
+print("look",word1,
+      word2,
+      word5,"a",
+      word3,"past the",
+      word4+".")
+      
+#input()
+word6=input("enter name")
+word7=input("enter verb")
+word8="painfully"
+word9="pool"
+word10="into"
+word11="the"
+print("watch",word6,
+      word8,
+      word7,
+      word10,
+      word11,
+      word9+".")
+input()
+
+    
+      

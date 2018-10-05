9412bb4f0b693d5ad446a3754c8d86ea17448659
Brandon Robbins <brandon.robbins@tooeleschools.org>
2018-10-05 13:18:43 -0600

    New Programs

---
+++ birb.checker.py
@@ -0,0 +1,13 @@
+
+
+def bird_available():
+        bird=input("enter a bird")
+        bird_types = "crow", "robin", "parrot", "eagle", "sandpiper", "hawk", "piegon"
+        if bird in bird_types:
+                print("yes")
+        else:
+                print("No")
+bird_available()
+
+
+    

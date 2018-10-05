dd2f936081070b6c6c0d4cb7b394ea054908a285
Brandon Robbins <brandon.robbins@tooeleschools.org>
2018-10-05 14:08:08 -0600

    fixed clock

--- Clock.py
+++ Clock.py
@@ -4,11 +4,18 @@
 from tkinter import*
 from tkinter import ttk
 from tkinter import font
+import winsound
 import time
 import calendar
 import datetime
-
-def current_time():
+h = 0
+m= 0
+s = 0
+t = "am"
+bgColor="Black"
+fgColor="Blue"
+bgLabColor="Black"
+def current_time(h,m,s,t):
     seconds = calendar.timegm(time.gmtime())
     current_seconds = seconds % 60
     minutes = seconds // 60
@@ -22,45 +29,56 @@ def current_time():
         tag="PM"
     else:
         tag="AM"
+    a = str(h)+":"+str(m)+":"+str(s)+t
     timex = str(current_hour)+":"+str(current_minutes)+":"+str(current_seconds)+tag
-##    if timex == a:
-##            beep()
+    if timex == a:
+         beep()
     return timex
-##def beep():
-##    winsound.Beep(560,8000)
+
+def beep():
+    winsound.Beep(560,8000)
     
 
 def quit(*args):
         root.destroy()
 def show_time():
-##    global h
-##    global m
-##    global s
-##    global t
-##        time=current_time()
-##        txt.set(time)
-        root.after(1000,show_time)
-##def get_alarm(*args):
-##    global h
-##    h= input("set hour")
-##    global m
-##    m= input("set min")
-##    global s
-##    s= input("set sec")
-##    global t
-##    t= input ("am or pm") .upper()
+    global h
+    global m
+    global s
+    global t
+    time=current_time(h,m,s,t)
+    txt.set(time)
+    root.after(1000, show_time)
+def get_alarm(*args):
+    global h
+    h= input("set hour")
+    global m
+    m= input("set min")
+    global s
+    s= input("set sec")
+    global t
+    t= input ("am or pm") .upper()
 ##    time=current_time()
 ##    txt.set(time)
 ##    root.after(1000, show_time)
+def change_color(*args):
+    global bgColor
+    global fgColor
+    global bgLabColor
+    bgColor = input("what color do you want the background").title()
+    fgColor = input("what color do you want the Clock text").title()
+    bgLabColor = bgColor
 
 root = Tk()
 root.attributes("-fullscreen", True)
-root.configure(background='Black')
+root.configure(background=bgColor)
 root.bind("x" ,quit)
+root.bind("a",get_alarm)
+root.bind("c",change_color)
 root.after(1000, show_time)
 fnt= font.Font(family= 'Fixedsys', size=100, weight='bold')
 txt=StringVar()
-lbl=ttk.Label(root, textvariable=txt, font=fnt, foreground="Orange", background ="Black")
+lbl=ttk.Label(root, textvariable=txt, font=fnt, foreground=fgColor, background =bgLabColor)
 lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
 root.mainloop()
         
